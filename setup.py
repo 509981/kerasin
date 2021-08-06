c7d13cb1272dda36f005937abcf64d2f35907a5d
509981 <88483677+509981@users.noreply.github.com>
2021-08-06 10:28:22 +1000

    Create setup.py

---
+++ setup.py
@@ -0,0 +1,11 @@
+from setuptools import setup
+
+setup(
+   name='kerasin',
+   version='1.0',
+   description='A useful module',
+   author='Dmitry Utenkov',
+   author_email='509981@gmail.com',
+   packages=['kerasin'],  #same as name
+   #install_requires=['bar', 'greek'], #external packages as dependencies
+)
