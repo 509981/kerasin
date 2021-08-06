a8b6578b37dbf210e1316e6c6b86e4ee421e60c6
509981 <88483677+509981@users.noreply.github.com>
2021-08-06 11:18:18 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,4 +1,5 @@
-from setuptools import setup
+#from setuptools import setup
+from distutils.core import setup
 
 setup(
    name='kerasin',
@@ -6,6 +7,7 @@ setup(
    description='A useful module',
    author='Dmitry Utenkov',
    author_email='509981@gmail.com',
-   packages=['kerasin'],  #same as name
+   #packages=['kerasin'],  #same as name
+   py_modules=['kerasin']
    #install_requires=['bar', 'greek'], #external packages as dependencies
 )
