eb19638f1d46e376e148c24b557057d83f7f228a
509981 <88483677+509981@users.noreply.github.com>
2021-08-08 18:00:58 +1000

    Update __init__.py

--- kerasin/__init__.py
+++ kerasin/__init__.py
@@ -1,1 +1,2 @@
-from kerasin.kerasin import addInput, addOutput, compile, fit, report, get_model, generate
+#from kerasin.kerasin import addInput, addOutput, compile, fit, report, get_model, generate
+from kerasin.kerasin import *
