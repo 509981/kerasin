b099a1ed1c169073448c459867c5c862771bdf57
509981 <88483677+509981@users.noreply.github.com>
2021-08-19 10:26:21 +1000

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,7 +8,5 @@ setup(
    description='Optimization keras model by GA',
    author='Dmitry Utenkov',
    author_email='509981@gmail.com',
-   packages=['kerasin'],  #same as name
-   #py_modules=['kerasin']
-   #install_requires=['bar', 'greek'], #external packages as dependencies
+   packages=['kerasin']
 )
